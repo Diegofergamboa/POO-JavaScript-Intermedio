2ba2ac421f6d150960909081c17fdbde5bac1320
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-30 18:00:27 -0500

    Update memory.js

--- memory.js
+++ memory.js
@@ -35,12 +35,30 @@ for (propiedad in objeto1) {
 // Craashea cuando tiene otro objeto.
 
 // Copia los objetos del objeto origiual
-const objeto3 = Object.asign({}, objeto1);
+const objeto3 = Object.assign({}, objeto1);
+// console.log(objeto3);
 
-const objetot4 = Object.create(objeto1);
+objeto3.a = 'Aqui cambio el valor del objeto 3, que es copiado desde el objeto1'
+// console.log(objeto1)
+
+objeto3.c.d = 'Aqui cambio el objeto 3 del objeto d que está en el c'
+// console.table(objeto3.c);
+// console.table(objeto1.c);
+// Como podemos ver, aquí también se cambia el valor del objeto 1, aunque hayasmos tenido en el objeto 3 de una manera normal con el assign. 
+
+// Eso si, el assign reemplaza al for que hicimos arriba.
+
 // Crea en proto una instancia con todas las propiedades del objeto 1.
 
 // Si cambiamos una de las propiedades la podremos cambiar porque están en proto
+
+const objeto4 = Object.create(objeto1);
+console.log(objeto4);
+
+
+objeto4.c.d = 'Aqui cambiando el valor del objeto d dentro del objeto c del objeto4'
+
+// Aqui las variables aunque se copien, vienen del objeto 1 y pasan a ser del prototype
 
 // Es con el .create como si crearamos que se colocaran nuevos objetos dentro de otros objetos, es decir colocaramos nuevas caracteristicas, unas cambiadas en proto y las otras en el objeto ordinario.
 
