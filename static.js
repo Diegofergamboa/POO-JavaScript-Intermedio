ddb55a133b19fc986126739ef0cd93c5deadcbd5
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-30 15:04:59 -0500

    Update static.js

--- static.js
+++ static.js
@@ -5,9 +5,24 @@ const juan = {
 
     addCourses(newCourse) {
         this.approvedCourses.push(newCourse);
+        // Con el this, estamos haciendo referencia al aproved courses de juan
     }
 };
 
-console.log(Object.keys(juan));
-console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan));
+/* Métodos con el super objeto Object */
+
+// para usar las propiedades estáticas del super objeto OBJECT.
+
+console.log(Object.keys(juan))
+// Cada una de las propiedades.
+console.log(Object.getOwnPropertyNames(juan))
+// Nos muestra un array con cada una de las propiedades del objeto juan.
+console.log(Object.entries(juan))
+// Es un array que nos muestra por medio de array cada uno de los atributos o propiedades.
+    Object.entries(juan)[3];
+    // nos muestra el array de add Course
+
+    
+
+
+
