0c153e817e54ad1c8a6413869259f5c48f3bd624
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-03 15:17:17 -0500

    Update recursividad.js

--- recursividad.js
+++ recursividad.js
@@ -1,8 +1,19 @@
-function recursiva(numero) {
-    console.log(numero);
-    if (numero < 5) {
-        return recursiva(numero + 1);
-    } else {
-        return 5;
-    }
-}
+// function recursiva(numero) {
+//     console.log(numero);
+//     if (numero < 5) {
+//         return recursiva(numero + 1);
+//     } else {
+//         return 5;
+//     }
+// }
+
+/* */ 
+
+// function recursiva() {
+//     if (/* Validación */) {
+//         //LLamados recursivos.
+//     } else {
+//         //LLamados normales sin recursividad.
+//     }
+// }
+
