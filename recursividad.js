1fbfe7ec4fbcf468876068b0d0d7b81e45d6d751
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-03 15:24:15 -0500

    Update recursividad.js

--- recursividad.js
+++ recursividad.js
@@ -18,8 +18,18 @@
 // }
 
 const numeros = [1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,2];
-let numero = 0;
-for (let index = 0 ; index < index.length ; index ++) {
-    numero = numeros[0]
-    console.log({index, numero});
+// let numero = 0;
+// for (let index = 0 ; index < index.length ; index ++) {
+//     numero = numeros[0]
+//     console.log({index, numero});
+// }
+
+function recursiva(numbersArray) {
+    
+    if (numbersArray.length != 0) {
+        const firstNumber = numbersArray[0];
+        console.log(firstNumber);
+        numbersArray.shift();
+        return recursiva(numbersArray);
+    }
 }
