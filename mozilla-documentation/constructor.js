88547e7815a1205896c0531170a1d84206528514
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-11 15:46:35 -0500

    Update constructor.js

--- mozilla-documentation/constructor.js
+++ mozilla-documentation/constructor.js
@@ -9,12 +9,20 @@ function Person(first, last, age, gender, interest) {
     this.gender = gender ,
     this. interest = interest;
     this.bio = function() { // Es más eficiente hacerlo con este método que con el método de crear el método con solo bio().
-        alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' and have interest on ' + this.interest[0] + ' and ' + this.interest[1]);
+        alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' and have interest on ' + showInterest() );
     };
     this.greeting = function () {
         alert('Hi, my name is ' + this.name.first + ' ' + this.name.last + ' . ' );
     };
+    function showInterest() {
+        let inter = [];
+        this.interest.forEach(element => {
+            inter += element
+        });
+        return inter;
+    }
 }
 
 // Voy a crear una instancia del Objeto.
 
+var person1 = new Person('Diego', 'Ortiz' , 25 , 'Male' , interest = ['Programming ' , 'Gym ' ,'Dance ','Jump .']);
