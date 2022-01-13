983b7847cef67feaa6c9dffe02cde02f5df207ac
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-13 13:50:21 -0500

    Object charged

--- mozilla-documentation/herencia.js
+++ mozilla-documentation/herencia.js
@@ -1,1 +1,22 @@
-alert('working on!')
+function Person(first, last, age, gender, interest) {
+    this.name = {
+        'first' : first ,
+        'last' : last ,
+    };
+    this.age = age ,
+    this.gender = gender ,
+    this. interest = interest;
+    this.bio = function() { // Es más eficiente hacerlo con este método que con el método de crear el método con solo bio().
+        alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' and have interest on ' + showInterest() );
+    };
+    this.greeting = function () {
+        alert('Hi, my name is ' + this.name.first + ' ' + this.name.last + ' . ' );
+    };
+    function showInterest() {
+        let inter = [];
+        this.interest.forEach(element => {
+            inter += element
+        });
+        return inter;
+    }
+}
