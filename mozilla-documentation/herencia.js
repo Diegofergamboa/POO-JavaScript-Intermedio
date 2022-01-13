c97b5a7b839fdbb0e7272e79f312de9b7b074da6
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-13 14:04:33 -0500

    heredar métodos y propiedades a través de call()
    Llamaa los método y propiedadesde una función constructora dentro de otro ámbito

--- mozilla-documentation/herencia.js
+++ mozilla-documentation/herencia.js
@@ -20,3 +20,26 @@ function Person(first, last, age, gender, interest) {
         return inter;
     }
 }
+
+// Aqui vamos a crear una clase profesor la cuàl va a heredar los métodos y propiedades de persona y va, a tener el al propiedad de asignatura
+
+// La función call() nos permite llamarotra función pero, en el contexto actual.
+
+function Profesor(first, last, age, gender, interest, materia) {
+    Person.call(this, first, last, age, gender, interest,) // Call es basicamente para poder llamar a unafunción que definimos en otro lugar, pero en el contexto actual
+    this.materia = materia
+}
+
+
+// Otro ejemplo
+
+function Pc(procesador, teclado) {
+    this.procesador = procesador ;
+    this.teclado = teclado ;
+}
+
+function ipad(procesador, teclado, widthScreen) {
+    Pc.call(this, procesador, teclado) ;
+    this.widthScreen = widthScreen ;
+}
+
