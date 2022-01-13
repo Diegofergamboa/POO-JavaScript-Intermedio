f04fb37e41930867fb1e5fd6f5114637a426361f
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-13 18:51:51 -0500

    Update platziAcademy.js

--- POO-intermedio/platziAcademy.js
+++ POO-intermedio/platziAcademy.js
@@ -2,3 +2,12 @@
 
 // Writable, configurable y enumerable; esto con el fín de poder acceder de una manera más adecuada a los Objetos.
 
+const juan = {
+    name: 'Juanito' ,
+    age: 18 ,
+    aprovedCourses: ['Curso 1'] ,
+    addCourse(newCourse) {
+        this.aprovedCourses.push(newCourse); // Con el this estamos referenciandonos al nuevo curso.
+    }
+}
+
