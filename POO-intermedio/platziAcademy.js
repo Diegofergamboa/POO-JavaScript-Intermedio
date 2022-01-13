087b1bd40459ea4aff4cfee5398797f5234e2b88
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-13 14:47:00 -0500

    directories to platzi academy

---
+++ POO-intermedio/platziAcademy.js
@@ -0,0 +1,4 @@
+// Entries, keys y, getownPropertyNames . Son métodos estáticos del súper objet Object.
+
+// Writable, configurable y enumerable; esto con el fín de poder acceder de una manera más adecuada a los Objetos.
+
