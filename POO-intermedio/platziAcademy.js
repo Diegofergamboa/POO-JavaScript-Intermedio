407db643530120baa8c87194d42a940677216b14
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-14 17:43:50 -0500

    Update platziAcademy.js

--- POO-intermedio/platziAcademy.js
+++ POO-intermedio/platziAcademy.js
@@ -7,7 +7,23 @@ const juan = {
     age: 18 ,
     aprovedCourses: ['Curso 1'] ,
     addCourse(newCourse) {
+        console.log('This', this)
+        console.log('This aproved courses', this.aprovedCourses)
         this.aprovedCourses.push(newCourse); // Con el this estamos referenciandonos al nuevo curso.
     }
 }
 
+console.log(Object.getOwnPropertyDescriptors(juan))
+
+
+// Para poder modificar configurable, enumerable y configurable.
+// juan['nuevaPropiedad'] = 'Nueva propiedad' // Es decir esto con el Object.
+
+Object.defineProperty(juan, 'pruebaNasa' ,{
+    value: 'extratrerrestres' ,
+    writable : true ,
+    configurable : true ,
+    enumerable : true ,
+})
+
+// Asignamos valores a propiedades y podemos definir valores de configurable, enumerable y, demás.
